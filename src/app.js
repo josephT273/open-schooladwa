8ddeb7125efdaae92ccd689ec2fd813b08a0866e
josepht273 <josephtadessem@gmail.com>
2024-08-01 00:11:15 +0300

    Setting up error handling methods

--- src/app.js
+++ src/app.js
@@ -3,3 +3,30 @@ import bodyParser from "body-parser";
 import methodOverride from "method-override";
 import cors from "cors";
 import "dotenv/config";
+
+// start express app
+const app = express();
+
+// Writing error handlers
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(bodyParser.json());
+app.use(methodOverride());
+app.use((err, req, res, next) => {
+  console.log(err.stack);
+  res.status(500).json({
+    message: "Something went wrong",
+  });
+});
+
+// Import the server port form env file
+const PORT = Number.parseInt(process.env.PORT) || 7000;
+
+// *Server
+app.listen(PORT, () => {
+  console.log(`🚀 Ther server started http://localhost:${PORT}`);
+});
