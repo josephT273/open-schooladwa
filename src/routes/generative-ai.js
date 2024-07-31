2da1c5bd0a775f1079e592fae88f91361d650e9d
josepht273 <josephtadessem@gmail.com>
2024-08-01 00:58:20 +0300

    welcome function

--- src/routes/generative-ai.js
+++ src/routes/generative-ai.js
@@ -10,16 +10,24 @@ const generativeAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const model = generativeAI.getGenerativeModel({
   model: "gemini-1.5-flash",
 });
+
+var welcomeMessage =
+  "Welcome to Google's AI API route! Use /chat to chat with gemini-1.5-flash.";
+const welcome = (req, res) => {
+  return res.send(welcomeMessage);
+};
+
 const chat = async (req, res) => {
   const { prompt } = req.body;
   const result = await model.generateContent(prompt);
 
-  res.status(200).json({
+  return res.status(200).json({
     statusCode: 200,
     data: result.response.text(),
   });
 };
 
+route.route("/").get(welcome);
 route.route("/chat").post(chat);
 
 export default route;
